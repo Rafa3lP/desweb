b68e88b90ccfbf5008a750a43474495958101200
Rafael de Andrade Prenholato <rafael.prenholato>
2023-04-13 23:20:09 -0300

    initial

---
+++ js/script.js
@@ -0,0 +1,62 @@
+function mudarFundo() {
+    const valor = parseInt(Math.random() * 10);
+    if (valor % 3 == 0) {
+        document.bgColor = "lightgreen";
+    } else {
+        document.bgColor = "lightgrey";
+    }
+}
+
+function resetar() {
+    location.reload();
+}
+
+function trocarTexto() {
+    const paragrafo = document.getElementById("texto");
+    paragrafo.innerHTML = "Texto modificado!";
+}
+
+function carregarImagem() {
+    const imgEl = document.getElementById("imagem");
+    imgEl.src = "imagens/arquitetura.png";
+}
+
+function desaparecerImagem() {
+    const imgEl = document.getElementById("imagem");
+    imgEl.src = "";
+}
+
+function ver() {
+    const info = document.getElementById("este");
+    info.style.display = "contents";
+}
+
+function esconder() {
+    const info = document.getElementById("este");
+    info.style.display = "none";
+}
+
+function tabelar() {
+    const tabela = document.getElementById("tabela");
+    tabela.border = "10";
+    tabela.style.backgroundColor = "goldenrod";
+    tabela.width = "70%";
+}
+
+function destacar() {
+    const campo = document.querySelector("input[type='text']");
+    campo.style.backgroundColor = "bisque";
+}
+
+function limpar() {
+    const campo = document.querySelector("input[type='text']");
+    campo.style.backgroundColor = "transparent";
+}
+
+function manipular() {
+    const lista = document.getElementById("lista");
+    const novoItem = document.createElement("li");
+    const texto = document.createTextNode("novo Item!!");
+    novoItem.appendChild(texto);
+    lista.appendChild(novoItem);
+}
